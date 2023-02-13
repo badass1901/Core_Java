b8c1e5ccc821e3aee4fd226f4eb548a0d571b1b6
badass1901 <shashwatsagar19@gmail.com>
2023-02-13 13:14:34 +0530

    lec10

---
+++ logical.java
@@ -0,0 +1,13 @@
+public class logical {
+     public static void main(String[] args)  {
+         System.out.println("hello");    
+         int a =10;
+         int b=22;
+         int x=5;
+         int y=0;
+         int c=9;
+         int d=6;
+          boolean result = a<b && (x<y || c<d);
+         System.out.println(result);
+        }
+}
