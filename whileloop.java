5271e0999329f00dde431deeb081c17257693a1a
badass1901 <shashwatsagar19@gmail.com>
2023-02-13 14:36:54 +0530

    lec17

---
+++ whileloop.java
@@ -0,0 +1,18 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i=0;
+
+        while(i<=4)
+        {
+            System.out.println("Hi " + i);
+            int j=1;
+                while(j<=3)
+                {
+                    System.out.println("Hello "+ j);
+                    j++;
+                }
+            i++;
+        }
+        
+    }
+}
