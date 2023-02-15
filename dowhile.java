dd0060cc4e2da63968b88b8e4419c1a3f3b7b2ee
Shashwat Sagar <59369746+badass1901@users.noreply.github.com>
2023-02-15 18:10:01 +0530

    lec18

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i = 5;
+        // while (i <= 4) {
+        // System.out.println("hi" + i);
+        // i++;
+        // }
+
+        do {
+            System.out.println("hi" + i);
+            i++;
+        } while (i <= 4);
+    }
+}
